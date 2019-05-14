cf056269519c9e01ae306ade2e0380ab11618262
Amitojasa <amitojvmc@gmail.com>
2019-05-14 17:18:33 +0530

    fetching data

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Table name
+    protected $table='posts';
+    // primary key
+    protected $primaryKey = 'id';
+    //time stamps
+    public $timestamps = 'true';
 }
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,11 +1,12 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
         <a class="navbar-brand" href="#">{{config('app.name','LSAPP')}}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-          <ul class="navbar-nav mr-auto">
+          <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link" href="/">Home</a>
             </li>
@@ -15,6 +16,10 @@
             <li class="nav-item">
               <a class="nav-link" href="/services">Services</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/posts">Blog</a>
+            </li>
           </ul>
         </div>
+      </div>
       </nav>
