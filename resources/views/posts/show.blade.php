a9dbb4e169c478b0a9f4500a22983a618d65ec68
Amitojasa <amitojvmc@gmail.com>
2019-05-18 16:38:19 +0530

    Edit and Delete post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <a href="/posts" class="btn btn-outline-dark">Go Back</a>
+    <a href="/posts" class="btn btn-outline-dark">Go Back</a> 
     <br><br>
     <h1>{{$post->title}}</h1>
     <div>
@@ -8,4 +8,10 @@
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-outline-dark">Edit</a>
+    {!! Form::open(['action' => ['PostsController@update',$post->id],'method'=>'POST','class'=>'float-right']) !!}
+        {{Form::hidden('_method','Delete')}}
+        {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+    {!! Form::close() !!}
 @endsection
